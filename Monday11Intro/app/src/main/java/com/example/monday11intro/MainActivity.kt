f8d17eaca5d2f129fd5615024c4af6593140243d
pontuscroneld <pontuscroneld@gmail.com>
2021-01-11 13:04:23 +0100

    Other activity

--- Monday11Intro/app/src/main/java/com/example/monday11intro/MainActivity.kt
+++ Monday11Intro/app/src/main/java/com/example/monday11intro/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.monday11intro
 
+
 import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -23,6 +27,8 @@ class MainActivity : AppCompatActivity() {
         var plusButton = findViewById<Button>(R.id.plusButton)
         var minusButton = findViewById<Button>(R.id.minusButton)
         var resetButton = findViewById<Button>(R.id.resetButton)
+        var setNumberButton = findViewById<Button>(R.id.setNumberButton)
+        var goOtherButton = findViewById<Button>(R.id.goOtherButton)
 
         plusButton.setOnClickListener {
             counterNumber = counterNumber + 1
@@ -30,7 +36,6 @@ class MainActivity : AppCompatActivity() {
             counterTextView.text = counterNumber.toString()
             counterPressed.text = count.toString()
         }
-
         minusButton.setOnClickListener {
 
             if(counterNumber <= 0){
@@ -42,13 +47,49 @@ class MainActivity : AppCompatActivity() {
             counterTextView.text = counterNumber.toString()
             counterPressed.text = count.toString()
         }
-
         resetButton.setOnClickListener {
-            counterNumber = 0
-            count = 0
+            resetCounting()
+        }
+        setNumberButton.setOnClickListener {
+            var numberEditText = findViewById<EditText>(R.id.numberEditText)
+            var enteredText = numberEditText.text.toString()
+            Log.d("PIA9DEBUG", enteredText)
+
+/*
+            var enteredNumber = enteredText.toIntOrNull()
+            if(enteredNumber != null) {
+                counterNumber = enteredNumber
+            }
             counterTextView.text = counterNumber.toString()
-            counterPressed.text = count.toString()
+*/
+
+            enteredText.toIntOrNull()?.let{
+                counterNumber = it
+                counterTextView.text = counterNumber.toString()
+            }
+
+            numberEditText.setText("")
+        }
+        goOtherButton.setOnClickListener {
+            Log.d("PIA9DEBUG", "Let's go")
+
+            var intent = Intent(this, OtherActivity::class.java)
+            intent.putExtra("counter", counterNumber)
+            startActivity(intent)
+
         }
     }
+
+    fun resetCounting()
+    {
+        counterNumber = 0
+        count = 0
+        var counterTextView = findViewById<TextView>(R.id.counterTextView)
+        var counterPressed = findViewById<TextView>(R.id.counterPressed)
+        counterTextView.text = counterNumber.toString()
+        counterPressed.text = count.toString()
+    }
+
+
 
 }
---
+++ Monday11Intro/app/src/main/java/com/example/monday11intro/OtherActivity.kt
@@ -0,0 +1,17 @@
+package com.example.monday11intro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class OtherActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_other)
+
+        var theNumber = intent.getIntExtra("counter", 0)
+        var otherTextView = findViewById<TextView>(R.id.otherTextView)
+
+        otherTextView.text = theNumber.toString()
+    }
+}
